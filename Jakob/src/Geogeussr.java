f7e34ba3ee77d624d6cc1fe9d44f10a3fdf25972
holml <holmloch@gmail.com>
2023-02-10 09:56:37 +0100

    Hello guys

--- Jakob/src/Geogeussr.java
+++ Jakob/src/Geogeussr.java
@@ -16,7 +16,7 @@ public class Geogeussr {
         lande.add("4. Australien");
         lande.add("5. Chile");
         lande.add("6. Kyrgyzstan");
-        lande.add("7. Tuvalu");
+        lande.add("7. Tuvalu!");
     }
 
     public void start() {

b0abc5c1c8779bfab083132eb9988cdf9dd1ac4f
Noermark999 <jakobnoermark@hotmail.com>
2023-02-10 09:41:42 +0100

    Jakob

---
+++ Jakob/src/Geogeussr.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Geogeussr {
+    private ArrayList<String> lande;
+
+    public Geogeussr() {
+        lande = new ArrayList<>();
+        init();
+    }
+
+    private void init() {
+        lande.add("1. Danmark");
+        lande.add("2. USA");
+        lande.add("3. Irland");
+        lande.add("4. Australien");
+        lande.add("5. Chile");
+        lande.add("6. Kyrgyzstan");
+    }
+
+    public void start() {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println(lande);
+            System.out.println("Skriv tallet på et land:");
+            String guess = scan.nextLine();
+            System.out.println("Du valgte: " + lande.get(Integer.parseInt(guess)-1));
+            }
+        }
+}
---
+++ Jakob/src/startMain.java
@@ -0,0 +1,6 @@
+public class startMain {
+    public static void main(String[] args) {
+        Geogeussr geogeussr = new Geogeussr();
+        geogeussr.start();
+    }
+}

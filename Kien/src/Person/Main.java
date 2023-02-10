3140c71117113f730e4bb74dd387cc5981741d16
JohnLe <johnle@hotmail.dk>
2023-02-10 10:39:06 +0100

    Kien

--- Kien/src/Person/Main.java
+++ Kien/src/Person/Main.java
@@ -5,8 +5,10 @@ import java.time.Period;
 
 public class Main {
     public static void main(String[] args) {
-    person Jakob = new person("Jakob",21, LocalDate.of(2001,3,24));
+        person Jakob = new person("Jakob", 21, LocalDate.of(2001, 3, 24));
+        person Kien = new person("Kien",24,LocalDate.of(1998,6,12));
         System.out.println(Jakob);
+
 
     }
 }
